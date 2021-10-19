76f0c2c6bb0c8f2935e4abae04ff7ac2f43f72e1
Emel Varol <58691727+emelvarol-cognite@users.noreply.github.com>
2021-10-19 14:39:59 +0200

    Refactor config file structure and add delete support on deploy (#5)

--- cognite/transformations_cli/commands/deploy/deploy.py
+++ cognite/transformations_cli/commands/deploy/deploy.py
@@ -7,13 +7,17 @@ from cognite.transformations_cli.commands.deploy.transformation_config import (
     TransformationConfigError,
     parse_transformation_configs,
 )
-from cognite.transformations_cli.commands.deploy.utils import (
-    create_notifications,
-    to_notifications,
+from cognite.transformations_cli.commands.deploy.transformations_api import (
+    get_existing_notifications_dict,
+    get_existing_schedules_dict,
+    get_existing_trasformation_ext_ids,
+    get_new_transformation_ids,
+    to_notification,
     to_schedule,
     to_transformation,
+    upsert_notifications,
     upsert_schedules,
-    upsert_transform,
+    upsert_transformations,
 )
 
 
@@ -23,8 +27,13 @@ from cognite.transformations_cli.commands.deploy.utils import (
     default=".",
     help="A directory to search for transformation manifests. If omitted, the current directory is used.",
 )
+@click.option(
+    "--debug",
+    is_flag=True,
+    help="Print updated, created, deleted external IDs ",
+)
 @click.pass_obj
-def deploy(obj: Dict, path: str) -> None:
+def deploy(obj: Dict, path: str, debug: bool = False) -> None:
     """
         Deploy a set of transformations from a directory
     Args:
@@ -32,18 +41,82 @@ def deploy(obj: Dict, path: str) -> None:
     """
     click.echo("Deploying transformation...")
     try:
-        transformation_configs = parse_transformation_configs(path)
-        transformations = [to_transformation(tr, obj["cluster"]) for tr in transformation_configs]
         _, exp_client = get_clients(obj)
-        upsert_transform(exp_client, transformations)
-        schedules = []
-        notifications = []
-        for transform in transformation_configs:
-            if transform.schedule:
-                schedules.append(to_schedule(transform))
-            if transform.notifications:
-                notifications = to_notifications(transform)
-        create_notifications(exp_client, notifications)
-        upsert_schedules(exp_client, schedules)
+        transformation_configs = parse_transformation_configs(path)
+        transformations = [to_transformation(t) for t in transformation_configs]
+        transformations_ext_ids = [t.external_id for t in transformation_configs]
+
+        existing_transformations_ext_ids = get_existing_trasformation_ext_ids(exp_client, transformations_ext_ids)
+        new_transformation_ext_ids = get_new_transformation_ids(
+            transformations_ext_ids, existing_transformations_ext_ids
+        )
+
+        existing_schedules_dict = get_existing_schedules_dict(exp_client, transformations_ext_ids)
+        existing_notifications_dict = get_existing_notifications_dict(exp_client, transformations_ext_ids)
+
+        requested_schedules_dict = {
+            t.external_id: to_schedule(t.external_id, t.schedule) for t in transformation_configs if t.schedule
+        }
+        requested_notifications_dict = dict()
+        for t in transformation_configs:
+            if t.notifications:
+                notifs = [to_notification(t.external_id, dest) for dest in t.notifications]
+                requested_notifications_dict[t.external_id] = notifs
+
+        _, updated_transformations, created_transformations = upsert_transformations(
+            exp_client, transformations, existing_transformations_ext_ids, new_transformation_ext_ids
+        )
+
+        deleted_schedules, updated_schedules, created_schedules = upsert_schedules(
+            exp_client,
+            existing_schedules_dict,
+            requested_schedules_dict,
+            existing_transformations_ext_ids,
+            new_transformation_ext_ids,
+        )
+
+        deleted_notifications, _, created_notifications = upsert_notifications(
+            exp_client,
+            existing_notifications_dict,
+            requested_notifications_dict,
+            existing_transformations_ext_ids,
+            new_transformation_ext_ids,
+        )
+
+        if updated_transformations:
+            click.echo(f"Number of transformations updated: {len(updated_transformations)}")
+            if debug:
+                click.echo("List of transformations updated:")
+                click.echo(updated_transformations)
+        if created_transformations:
+            click.echo(f"Number of transformations created: {len(created_transformations)}")
+            if debug:
+                click.echo("List of transformations created:")
+                click.echo(created_transformations)
+        if deleted_schedules:
+            click.echo(f"Number of schedules deleted: {len(deleted_schedules)}")
+            if debug:
+                click.echo("List of schedules deleted:")
+                click.echo(deleted_schedules)
+        if updated_schedules:
+            click.echo(f"Number of schedules updated: {len(updated_schedules)}")
+            if debug:
+                click.echo("List of schedules updated:")
+                click.echo(updated_schedules)
+        if created_schedules:
+            click.echo(f"Number of schedules created: {len(created_schedules)}")
+            if debug:
+                click.echo("List of schedules created:")
+                click.echo(created_schedules)
+        if deleted_notifications:
+            click.echo(f"Number of notificatons deleted: {len(deleted_notifications)}")
+            if debug:
+                click.echo("List of notifications deleted:")
+                click.echo(deleted_notifications)
+        if created_notifications:
+            click.echo(f"Number of notifications created: {len(created_notifications)}")
+            if debug:
+                click.echo("List of notifications created:")
+                click.echo(created_notifications)
     except TransformationConfigError as e:
         exit(e.message)
---
+++ cognite/transformations_cli/commands/deploy/transformations_api.py
@@ -0,0 +1,241 @@
+import sys
+from typing import Dict, List, Optional, Tuple, Union
+
+from cognite.client.exceptions import CogniteAPIError
+from cognite.experimental import CogniteClient as ExpCogniteClient
+from cognite.experimental.data_classes import TransformationNotification
+from cognite.experimental.data_classes.transformation_schedules import TransformationSchedule
+from cognite.experimental.data_classes.transformations import (
+    OidcCredentials,
+    RawTable,
+    Transformation,
+    TransformationDestination,
+)
+
+from cognite.transformations_cli.commands.deploy.transformation_config import (
+    ActionType,
+    AuthConfig,
+    DestinationConfig,
+    DestinationType,
+    QueryConfig,
+    ReadWriteAuthentication,
+    TransformationConfig,
+)
+from cognite.transformations_cli.commands.utils import exit_with_cognite_api_error
+
+
+def to_transformation(config: TransformationConfig, cluster: str = "europe-west1-1") -> Transformation:
+    return Transformation(
+        name=config.name,
+        external_id=config.external_id,
+        destination=to_destination(config.destination),
+        conflict_mode=to_action(config.action),
+        is_public=config.shared,
+        ignore_null_fields=config.ignore_null_fields,
+        query=to_query(config.query),
+        source_api_key=to_read_api_key(config.authentication),
+        destination_api_key=to_write_api_key(config.authentication),
+        source_oidc_credentials=to_read_oidc(config.authentication, cluster),
+        destination_oidc_credentials=to_write_oidc(config.authentication, cluster),
+    )
+
+
+def to_action(action: ActionType) -> str:
+    return "abort" if action == ActionType.create else action.value
+
+
+def to_destination(destination: DestinationConfig) -> TransformationDestination:
+    if destination.type == DestinationType.raw:
+        destination = RawTable("raw", destination.raw_database, destination.raw_table)
+    else:
+        destination = TransformationDestination(destination.type.value)
+
+
+def to_query(query: Union[str, QueryConfig]) -> str:
+    try:
+        if isinstance(query, QueryConfig):
+            with open(query.file, "r") as f:
+                return f.read().replace("\n", "")
+        return query
+    except:
+        sys.exit("Please provide a valid path for sql file.")
+
+
+def to_read_api_key(authentication: Union[AuthConfig, ReadWriteAuthentication]) -> Optional[str]:
+    if isinstance(authentication, AuthConfig):
+        return authentication.api_key
+    if isinstance(authentication, ReadWriteAuthentication):
+        return authentication.read.api_key
+    return None
+
+
+def to_write_api_key(authentication: Union[AuthConfig, ReadWriteAuthentication]) -> Optional[str]:
+    if isinstance(authentication, AuthConfig):
+        return authentication.api_key
+    if isinstance(authentication, ReadWriteAuthentication):
+        return authentication.write.api_key
+    return None
+
+
+def get_default_scopes(scopes: Optional[List[str]], cluster: str) -> str:
+    return ",".join(scopes) if scopes else f"https://{cluster}.cognitedata.com/.default"
+
+
+def is_oidc_defined(auth_config: AuthConfig) -> bool:
+    return (
+        auth_config.token_client_id
+        and auth_config.token_client_secret
+        and auth_config.token_url
+        and auth_config.token_project
+    )
+
+
+def get_oidc(auth_config: AuthConfig, cluster: str) -> Optional[OidcCredentials]:
+    scopes = auth_config.token_scopes if auth_config.audience else get_default_scopes(auth_config.token_scopes, cluster)
+    return (
+        OidcCredentials(
+            auth_config.token_client_id,
+            auth_config.token_client_secret,
+            scopes,
+            auth_config.token_url,
+            auth_config.token_project,
+            auth_config.audience,
+        )
+        if is_oidc_defined(auth_config)
+        else None
+    )
+
+
+def to_read_oidc(authentication: Union[AuthConfig, ReadWriteAuthentication], cluster: str) -> Optional[OidcCredentials]:
+    return (
+        get_oidc(authentication, cluster)
+        if isinstance(authentication, AuthConfig)
+        else get_oidc(authentication.read, cluster)
+    )
+
+
+def to_write_oidc(
+    authentication: Union[AuthConfig, ReadWriteAuthentication], cluster: str
+) -> Optional[OidcCredentials]:
+    return (
+        get_oidc(authentication, cluster)
+        if isinstance(authentication, AuthConfig)
+        else get_oidc(authentication.write, cluster)
+    )
+
+
+def to_schedule(transformation_external_id: str, interval: str) -> TransformationSchedule:
+    return TransformationSchedule(external_id=transformation_external_id, interval=interval)
+
+
+def to_notification(transformation_external_id: str, destination: str) -> TransformationNotification:
+    return TransformationNotification(transformation_external_id=transformation_external_id, destination=destination)
+
+
+def get_existing_trasformation_ext_ids(exp_client: ExpCogniteClient, all_ext_ids: List[str]) -> List[str]:
+    return [
+        t.external_id
+        for t in exp_client.transformations.retrieve_multiple(external_ids=all_ext_ids, ignore_unknown_ids=True)
+    ]
+
+
+def get_new_transformation_ids(all_ext_ids: List[str], existig_ext_ids: List[str]) -> List[str]:
+    return list(set(all_ext_ids) - set(existig_ext_ids))
+
+
+def get_existing_schedules_dict(
+    exp_client: ExpCogniteClient, all_ext_ids: List[str]
+) -> Dict[str, TransformationSchedule]:
+    return {
+        s.external_id: s
+        for s in exp_client.transformations.schedules.retrieve_multiple(
+            external_ids=all_ext_ids, ignore_unknown_ids=True
+        )
+    }
+
+
+def get_existing_notifications_dict(
+    exp_client: ExpCogniteClient, all_ext_ids: List[str]
+) -> Dict[str, List[TransformationNotification]]:
+    existing_notifications = dict()
+    for ext_id in all_ext_ids:
+        notifications = exp_client.transformations.notifications.list(transformation_external_id=ext_id, limit=-1)
+        if notifications:
+            existing_notifications[ext_id] = notifications
+    return existing_notifications
+
+
+def upsert_transformations(
+    exp_client: ExpCogniteClient,
+    transformations: List[Transformation],
+    existing_ext_ids: List[str],
+    new_ext_ids: List[str],
+) -> Tuple[List[str], List[str], List[str]]:
+    try:
+        updated = exp_client.transformations.update(
+            [tr for tr in transformations if tr.external_id in existing_ext_ids]
+        )
+        created = exp_client.transformations.create([tr for tr in transformations if tr.external_id in new_ext_ids])
+        return [], [t.external_id for t in updated], [t.external_id for t in created]
+    except CogniteAPIError as e:
+        exit_with_cognite_api_error(e)
+    return [], [], []
+
+
+def upsert_schedules(
+    exp_client: ExpCogniteClient,
+    existing_schedules_dict: Dict[str, TransformationSchedule],
+    requested_schedules_dict: Dict[str, TransformationSchedule],
+    existing_transformations_ext_ids: List[str],
+    new_transformations_ext_ids: List[str],
+) -> Tuple[List[str], List[str], List[str]]:
+    to_delete = []
+    to_update = []
+    to_create = []
+    try:
+        for ext_id in existing_transformations_ext_ids:
+            if ext_id in existing_schedules_dict and ext_id not in requested_schedules_dict:
+                to_delete.append(ext_id)
+            elif ext_id in existing_schedules_dict:
+                to_update.append(ext_id)
+            elif ext_id in requested_schedules_dict:
+                to_create.append(ext_id)
+        to_create += [ext_id for ext_id in new_transformations_ext_ids if ext_id in requested_schedules_dict]
+        exp_client.transformations.schedules.delete(external_id=to_delete)
+        exp_client.transformations.schedules.update([requested_schedules_dict[ext_id] for ext_id in to_update])
+        exp_client.transformations.schedules.create([requested_schedules_dict[ext_id] for ext_id in to_create])
+    except CogniteAPIError as e:
+        exit_with_cognite_api_error(e)
+    return to_delete, to_update, to_create
+
+
+def upsert_notifications(
+    exp_client: ExpCogniteClient,
+    existing_notifications_dict: Dict[str, List[TransformationNotification]],
+    requested_notifications_dict: Dict[str, List[TransformationNotification]],
+    existing_transformations_ext_ids: List[str],
+    new_transformations_ext_ids: List[str],
+) -> Tuple[List[Tuple[str, str]], List[Tuple[str, str]], List[Tuple[str, str]]]:
+    try:
+        to_delete = dict()
+        to_create: List[TransformationNotification] = [
+            requested_notifications_dict[ext_id]
+            for ext_id in new_transformations_ext_ids
+            if ext_id in requested_notifications_dict
+        ]
+        for ext_id in existing_transformations_ext_ids:
+            existing_notif = existing_notifications_dict.get(ext_id, [])
+            requested_notif = requested_notifications_dict.get(ext_id, [])
+            existing_destinations = [e.destination for e in existing_notif]
+            requested_destinations = [e.destination for e in requested_notif]
+
+            to_delete.update(
+                {e.id: (ext_id, e.destination) for e in existing_notif if e.destination not in requested_destinations}
+            )
+            to_create += [e for e in requested_notif if e.destination not in existing_destinations]
+        exp_client.transformations.notifications.delete(list(to_delete.keys()))
+        exp_client.transformations.notifications.create(to_create)
+        return list(to_delete.values()), [], [(n.transformation_external_id, n.destination) for n in to_create]
+    except CogniteAPIError as e:
+        exit_with_cognite_api_error(e)
+    return [], [], []
