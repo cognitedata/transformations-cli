2d5f29690dc0942bf406b1e0f2a602bd452d605e
Emel Varol <emel.varol@cognite.com>
2021-10-29 21:24:16 +0200

    Test delete command

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -1,17 +1,64 @@
-from typing import List
+import uuid
 
 import pytest
+from click.testing import CliRunner
 from cognite.experimental import CogniteClient as ExpCogniteClient
-from cognite.experimental.data_classes.transformations import Transformation
+from cognite.experimental.data_classes.transformations import Transformation, TransformationDestination
+
+from cognite.transformations_cli.commands.base import transformations_cli
 
 
 @pytest.mark.unit
-def test_delete(
-    exp_client: ExpCogniteClient,
-    test_transformation_ext_ids: List[str],
-    configs_to_create: List[Transformation],
-) -> None:
-    # Clean up before the test
-    exp_client.transformations.schedules.delete(external_id=test_transformation_ext_ids, ignore_unknown_ids=True)
-    exp_client.transformations.delete(external_id=test_transformation_ext_ids, ignore_unknown_ids=True)
-    assert True
+def test_delete_by_id(exp_client: ExpCogniteClient) -> None:
+    uuid_conf = str(uuid.uuid1())
+    created = exp_client.transformations.create(
+        Transformation(
+            external_id=uuid_conf,
+            name=uuid_conf,
+            query="select * from asd",
+            destination=TransformationDestination.assets(),
+            conflict_mode="upsert",
+        )
+    )
+    assert exp_client.transformations.retrieve(created.id) is not None
+    runner = CliRunner()
+    result = runner.invoke(transformations_cli, ["delete", f"--id={created.id}"])
+    assert result.exit_code == 0
+    assert f"Successfully deleted the transformation with id {created.id}.\n" == result.output
+    assert exp_client.transformations.retrieve(created.id) is None
+
+
+@pytest.mark.unit
+def test_delete_by_external_id(exp_client: ExpCogniteClient) -> None:
+    uuid_conf = str(uuid.uuid1())
+    exp_client.transformations.create(
+        Transformation(
+            external_id=uuid_conf,
+            name=uuid_conf,
+            query="select * from asd",
+            destination=TransformationDestination.assets(),
+            conflict_mode="upsert",
+        )
+    )
+    assert exp_client.transformations.retrieve(external_id=uuid_conf) is not None
+    runner = CliRunner()
+    result = runner.invoke(transformations_cli, ["delete", f"--external-id={uuid_conf}"])
+    assert result.exit_code == 0
+    assert f"Successfully deleted the transformation with external id {uuid_conf}.\n" == result.output
+    assert exp_client.transformations.retrieve(external_id=uuid_conf) is None
+
+
+@pytest.mark.unit
+def test_delete_by_both_ids() -> None:
+    runner = CliRunner()
+    result = runner.invoke(transformations_cli, ["delete", "--external-id=asd", "--id=1"])
+    assert result.exit_code == 1
+    assert result.exc_info[1].args[0] == "Please only provide one of id and external id."
+
+
+@pytest.mark.unit
+def test_delete_by_no_ids() -> None:
+    runner = CliRunner()
+    result = runner.invoke(transformations_cli, ["delete"])
+    assert result.exit_code == 1
+    assert result.exc_info[1].args[0] == "Please provide one of id and external id."
