792dcf8a77478dd0f61d88297199d8ee3641af66
Einar Omang <einar@omang.com>
2021-11-12 13:03:53 +0100

    Simple pagination for listing

--- cognite/transformations_cli/commands/jobs.py
+++ cognite/transformations_cli/commands/jobs.py
@@ -16,20 +16,35 @@ from cognite.transformations_cli.commands.utils import (
 @click.option("--id", help="List jobs by transformation id. Either this or --external-id must be specified.")
 @click.option("--external-id", help="List jobs by transformation external_id. Either this or --id must be specified.")
 @click.option("--limit", default=10, help="Limit for the job history, defaults to 10. Use -1 to retrieve all results.")
+@click.option("-i", "--interactive", is_flag=True, help="Display only 10 jobs at a time, paging through them.")
 @click.pass_obj
-def jobs(obj: Dict, id: Optional[int], external_id: Optional[str], limit: int = 10) -> None:
+def jobs(obj: Dict, id: Optional[int], external_id: Optional[str], limit: int = 10, interactive: bool = False) -> None:
     _, exp_client = get_clients(obj)
     is_id_exclusive(id, external_id)
     try:
+        id_str = ""
         if id:
-            click.echo(f"Listing the latest jobs for transformation with id {id}:")
+            id_str = f"id {id}"
         if external_id:
             id = get_id_from_external_id(exp_client=exp_client, external_id=external_id)
-            click.echo(f"Listing the latest jobs for transformation with external_id {external_id}:")
+            id_str = f"id: {external_id}"
+        
+        click.echo(f"Listing the latest jobs for transformation with {id_str}:")
         jobs = exp_client.transformations.jobs.list(limit=limit, transformation_id=id)
+
         if jobs:
-            click.echo("Resulting jobs:")
-            click.echo(print_jobs(jobs))
+            if interactive:
+                chunks = [jobs[i:i + 10] for i in range(0, len(jobs), 10)]
+                for chunk in chunks:
+                    click.clear()
+                    click.echo(f"Resulting jobs for transformation with {id_str}:")
+                    click.echo(print_jobs(chunk))
+                    ch = input("Press Enter to continue, q to quit ")
+                    if ch == "q":
+                        return
+            else:
+                click.echo(f"Resulting jobs for transformation with {id_str}:")
+                click.echo(print_jobs(jobs))
         else:
             click.echo("No jobs to list.")
     except (CogniteNotFoundError, CogniteAPIError) as e:
--- cognite/transformations_cli/commands/list.py
+++ cognite/transformations_cli/commands/list.py
@@ -2,19 +2,31 @@ from typing import Dict
 
 import click
 from cognite.client.exceptions import CogniteAPIError
+from cognite.experimental.data_classes.transformations import TransformationList
 
 from cognite.transformations_cli.clients import get_clients
 from cognite.transformations_cli.commands.utils import print_transformations
 
+import sys
 
 @click.command(help="List transformations")
 @click.option("--limit", default=10, help="Number of transformations to list, defaults to 10. Use -1 to list all.")
+@click.option("-i", "--interactive", is_flag=True, help="Display only 10 transformations at a time, then page through them.")
 @click.pass_obj
 # TODO idea: make this interactive, user can go back and forth through pages
-def list(obj: Dict, limit: int = 10) -> None:
+def list(obj: Dict, limit: int = 10, interactive: bool = False) -> None:
     _, exp_client = get_clients(obj)
     try:
         transformations = exp_client.transformations.list(limit=limit)
-        click.echo(print_transformations(transformations))
+        if interactive:
+            chunks = [transformations[i:i + 10] for i in range(0, len(transformations), 10)]
+            for chunk in chunks:
+                click.clear()
+                click.echo(print_transformations(chunk))
+                ch = input("Press Enter to continue, q to quit ")
+                if ch == "q":
+                    return
+        else:
+            click.echo(print_transformations(transformations))
     except CogniteAPIError as e:
         exit(f"Cognite API error has occurred: {e}")
