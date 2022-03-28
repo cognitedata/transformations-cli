45d681f58183e026086b67d8df4a7aed8c5ff8c9
Emel Varol <58691727+emelvarol-cognite@users.noreply.github.com>
2022-03-28 10:17:49 +0200

    Support sequence rows and filter by data set ids (#49)

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -12,6 +12,8 @@ from cognite.client.data_classes import (
     TransformationNotification,
     TransformationSchedule,
 )
+from cognite.client.data_classes.transformations._alphatypes import AlphaDataModelInstances
+from cognite.client.data_classes.transformations.common import SequenceRows
 
 from cognite.transformations_cli.commands.deploy.deploy import deploy
 from cognite.transformations_cli.commands.deploy.transformations_api import (
@@ -20,16 +22,6 @@ from cognite.transformations_cli.commands.deploy.transformations_api import (
     upsert_transformations,
 )
 from tests.test_config import rmdir, write_config
-
-
-@pytest.fixture
-def new_dataset(client: CogniteClient) -> DataSet:
-    ds_ext_id1 = "cli-transformation-ds"
-    ds1 = client.data_sets.retrieve(external_id=ds_ext_id1)
-    if not ds1:
-        data_set1 = DataSet(name=ds_ext_id1, external_id=ds_ext_id1)
-        ds1 = client.data_sets.create(data_set1)
-    yield ds1
 
 
 @pytest.mark.parametrize(
@@ -129,6 +121,72 @@ def test_deploy_with_ds_and_run(
     # run the deployed transformation to check the validity
     job = new_conf.run(wait=True)
     assert job.status == TransformationJobStatus.COMPLETED
+    client.transformations.delete(external_id=external_id, ignore_unknown_ids=True)
+    rmdir(Path(test_name))
+
+
+def test_deploy_dmi_transformation(
+    cli_runner: CliRunner, obj: Dict[str, Optional[str]], new_dataset: DataSet, client: CogniteClient
+) -> None:
+    test_name = "test_deploy_"
+    external_id = str(uuid.uuid1())
+    file = f"""
+        externalId: {external_id}
+        name: {external_id}
+        query: select 'test' as key, 'test' as name
+        authentication:
+            clientId: ${{CLIENT_ID}}
+            clientSecret: ${{CLIENT_SECRET}}
+            tokenUrl: "https://login.microsoftonline.com/b86328db-09aa-4f0e-9a03-0136f604d20a/oauth2/v2.0/token"
+            scopes:
+                - "https://bluefield.cognitedata.com/.default"
+            cdfProjectName: "extractor-bluefield-testing"
+        destination:
+            type: data_model_instances
+            externalId: test_model
+        shared: true
+        ignoreNullFields: False
+        action: upsert
+        """
+    write_config(test_name, file, 0)
+    cli_result = cli_runner.invoke(deploy, [test_name], obj=obj)
+    assert cli_result.exit_code == 0
+    new_conf = client.transformations.retrieve(external_id=external_id)
+    assert new_conf.external_id == external_id
+    assert new_conf.destination == AlphaDataModelInstances("test_model")
+    client.transformations.delete(external_id=external_id, ignore_unknown_ids=True)
+    rmdir(Path(test_name))
+
+
+def test_deploy_sequence_rows_transformation(
+    cli_runner: CliRunner, obj: Dict[str, Optional[str]], new_dataset: DataSet, client: CogniteClient
+) -> None:
+    test_name = "test_deploy_"
+    external_id = str(uuid.uuid1())
+    file = f"""
+        externalId: {external_id}
+        name: {external_id}
+        query: select 'test' as key, 'test' as name
+        authentication:
+            clientId: ${{CLIENT_ID}}
+            clientSecret: ${{CLIENT_SECRET}}
+            tokenUrl: "https://login.microsoftonline.com/b86328db-09aa-4f0e-9a03-0136f604d20a/oauth2/v2.0/token"
+            scopes:
+                - "https://bluefield.cognitedata.com/.default"
+            cdfProjectName: "extractor-bluefield-testing"
+        destination:
+            type: sequence_rows
+            externalId: test_sequence
+        shared: true
+        ignoreNullFields: False
+        action: upsert
+        """
+    write_config(test_name, file, 0)
+    cli_result = cli_runner.invoke(deploy, [test_name], obj=obj)
+    assert cli_result.exit_code == 0
+    new_conf = client.transformations.retrieve(external_id=external_id)
+    assert new_conf.external_id == external_id
+    assert new_conf.destination == SequenceRows("test_sequence")
     client.transformations.delete(external_id=external_id, ignore_unknown_ids=True)
     rmdir(Path(test_name))
 
