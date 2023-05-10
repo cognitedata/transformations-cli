3a03c3160f11c239d48bf34bd6468c374bd64081
SunilVenekarKrishnamoorthy <77153672+SunilVenekarKrishnamoorthy@users.noreply.github.com>
2023-05-10 15:17:27 +0200

    Update clients.py

--- cognite/transformations_cli/clients.py
+++ cognite/transformations_cli/clients.py
@@ -9,7 +9,6 @@ from cognite.client.credentials import OAuthClientCredentials
 logger = logging.getLogger(name=None)
 
 def get_client(obj: Dict, timeout: int = 60) -> CogniteClient:
-    api_key = obj.get("api_key")
     client_id = obj.get("client_id")
     client_secret = obj.get("client_secret")
     token_url = obj.get("token_url")
@@ -18,48 +17,23 @@ def get_client(obj: Dict, timeout: int = 60) -> CogniteClient:
     cdf_project_name = obj.get("cdf_project_name")
     cluster = obj.get("cluster", "europe-west1-1")
     base_url = f"https://{cluster}.cognitedata.com"
-    if not api_key and not audience:
+    if not audience:
         scopes = scopes.strip().split(" ") if scopes else [f"https://{cluster}.cognitedata.com/.default"]
     try:
-        if api_key is not None and (
-            client_id is not None
-            or client_secret is not None
-            or token_url is not None
-            or scopes is not None
-            or audience is not None
-        ):
-            sys.exit("Please provide only API key configuration or only OAuth2 configuration.")
-        elif api_key is not None:
-            client_config = ClientConfig(
-                client_name="transformations_cli",
-                base_url=base_url,
-                project=cdf_project_name,
-                timeout=timeout,
-                credentials=APIKey(api_key),
-            )
-            client = CogniteClient(client_config)
-            prj = get_project_from_api_key(client)
-            if not cdf_project_name:
-                logger.warn("CDF project name is not provided, it will be detected using the API key.")
-                client.config.project = prj
-            elif prj.lower() != cdf_project_name.lower():
-                sys.exit(f"API key does not grant access to the project {cdf_project_name}.")
-            return client
-        else:
-            token_custom_args = {"audience": audience} if audience else {}
-            client_config = ClientConfig(
-                base_url=base_url,
-                client_name="transformations_cli",
-                project=cdf_project_name,
-                timeout=timeout,
-                credentials=OAuthClientCredentials(
-                    client_id=client_id,
-                    client_secret=client_secret,
-                    token_url=token_url,
-                    scopes=scopes,
-                    **token_custom_args,
-                ),
-            )
+        token_custom_args = {"audience": audience} if audience else {}
+        client_config = ClientConfig(
+            base_url=base_url,
+            client_name="transformations_cli",
+            project=cdf_project_name,
+            timeout=timeout,
+            credentials=OAuthClientCredentials(
+                client_id=client_id,
+                client_secret=client_secret,
+                token_url=token_url,
+                scopes=scopes,
+                **token_custom_args,
+            ),
+        )
             return CogniteClient(client_config)
     except CogniteAPIError as e:
         sys.exit(f"Cognite client cannot be initialised: {e}.")
