a91b287d8d351c50680218d40f367640b3eb2b11
Nimesha Wjewardana <109508509+nimeshawij@users.noreply.github.com>
2023-05-12 15:29:22 +0530

    renamed InstanceNodes, InstanceEdges to Nodes and Edges

--- cognite/transformations_cli/commands/deploy/transformations_api.py
+++ cognite/transformations_cli/commands/deploy/transformations_api.py
@@ -13,8 +13,8 @@ from cognite.client.data_classes import (
 )
 from cognite.client.data_classes.transformations.common import (
     EdgeType,
-    InstanceEdges,
-    InstanceNodes,
+    Edges,
+    Nodes,
     SequenceRows,
     ViewInfo,
 )
@@ -105,7 +105,7 @@ def to_destination(destination: DestinationConfigType) -> TransformationDestinat
                 destination.view.external_id,
                 destination.view.version,
             )
-        return InstanceNodes(view, destination.instance_space)
+        return Nodes(view, destination.instance_space)
 
     elif isinstance(destination, InstanceEdgesDestinationConfig):
         view = None
@@ -118,7 +118,7 @@ def to_destination(destination: DestinationConfigType) -> TransformationDestinat
         edge_type = None
         if destination.edge_type:
             edge_type = EdgeType(destination.edge_type.space, destination.edge_type.external_id)
-        return InstanceEdges(view, destination.instance_space, edge_type)
+        return Edges(view, destination.instance_space, edge_type)
 
     else:
         return TransformationDestination(destination.value)
